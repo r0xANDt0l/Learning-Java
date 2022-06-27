ed024c82b91cfc974b877f2680a3956e729a3aa9
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-06-27 16:14:41 +0200

    added stuf

---
+++ src/com/rodrigo/Main.java
@@ -0,0 +1,15 @@
+package com.rodrigo;
+
+//Classes -> PascalNamingConvention
+//Methods -> camelNamingConvention
+
+//Every line ends with ;, just like in almost every language
+
+public class Main {
+
+    public static void main(String[] args){
+        //Code!
+        System.out.println("Hola Mundo!");
+    }
+
+}
